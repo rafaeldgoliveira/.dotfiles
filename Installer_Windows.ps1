a1952bce34ccaf8868d0d9167a48001348d29c36
Rafael Diego Gomes de Oliveira <rafadiego.gomes@gmail.com>
2021-03-30 12:18:37 -0300

    Update Installer_Windows.ps1

--- Installer_Windows.ps1
+++ Installer_Windows.ps1
@@ -1,16 +1,30 @@
 #Requires -RunAsAdministrator
 
-Write-Host "Antes de prosseguir com script é necessário instalar pela Microsoft Store:" -ForegroundColor Green
-Write-Host "Windows Terminal Preview" -ForegroundColor Yellow
-Write-Host ""
-Write-Host "-----------------------------------------"
-Write-Host "Aplicativos sugeridos:" -ForegroundColor Green
-Write-Host "Seu Telefone" -ForegroundColor Yellow
-Write-Host "Alienware Command Center" -ForegroundColor Yellow
-Write-Host "-----------------------------------------"
-Write-Host ""
-Write-Host "Aperte qualquer tecla para continuar..." -ForegroundColor Green
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Write-Host "Instale os seguintes aplicativos pela Microsoft Store:" -ForegroundColor Green
+Write-Host "	Windows Terminal" -ForegroundColor Yellow
+Write-Host "	Seu Telefone" -ForegroundColor Yellow
+$title   = '-----------------------------------------'
+$msg     = 'Já instalou os aplicativos?'
+$options = '&S', '&N'
+$default = 1  # 0=S, 1=N
+
+do {
+    $response = $Host.UI.PromptForChoice($title, $msg, $options, $default)
+} until ($response -eq 0)
+
+function Reload-Profile {
+    @(
+        $Profile.AllUsersAllHosts,
+        $Profile.AllUsersCurrentHost,
+        $Profile.CurrentUserAllHosts,
+        $Profile.CurrentUserCurrentHost
+    ) | % {
+        if(Test-Path $_){
+            Write-Verbose "Running $_"
+            . $_
+        }
+    }    
+}
 
 # Renomeia o computador
 Write-Host "Renomeando o computador para 'Rafael-Windows'" -ForegroundColor Green
@@ -23,88 +37,85 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # Instala os pacotes
 Write-Host "Instalando os pacotes do Chocolatey" -ForegroundColor Green
 choco install 7zip `
-							adoptopenjdk8 `
-							androidstudio `
-							anki `
-							autohotkey `
-							bulkrenameutility -y --ignore-checks `
-							calibre `
-							ccleaner `
-							choco-cleaner `
-							choco-upgrade-all-at-startup `
-							chrome-remote-desktop-chrome `
-							cpu-z `
-							crystaldiskmark `
-							ddu `
-							discord `
-							docker-cli `
-							docker-compose `
-							docker-desktop `
-							dropbox `
-							ds4windows `
-							epicgameslauncher `
-							evernote `
-							ffmpeg-batch `
-							firacodenf `
-							firefox `
-							freefilesync `
-							fzf `
-							logitech-options `
-							gamesavemanager `
-							geforce-experience `
-							git `
-							google-backup-and-sync `
-							googlechrome `
-							gpu-z `
-							gradle `
-							gsudo `
-							handbrake `
-							hwmonitor `
-							icloud `
-							insomnia-rest-api-client `
-							irfanview `
-							itunes `
-							lame `
-							libreoffice-fresh `
-							linkshellextension `
-							lockhunter `
-							megasync -y --ignore-check `
-							mo2 `
-							mp3gain `
-							mp3gain-gui `
-							mp3tag `
-							nodejs-lts `
-							partitionwizard `
-							picpick.portable `
-							powershell-core `
-							powertoys `
-							processhacker `
-							python3 `
-							recuva `
-							reflect-free `
-							remove-empty-directories `
-							revo-uninstaller `
-							samsung-magician -y --ignore-checks `
-							samsung-nvme-driver `
-							samsung-usb-driver `
-							spotify `
-							steam `
-							stremio `
-							transmission `
-							treesizefree `
-							twitch -y --ignore-checks `
-							unchecky `
-							veracrypt `
-							virtualbox `
-							virtualbox-guest-additions-guest.install `
-							vlc `
-							vscode `
-							whatsapp `
-							win32diskimager	 `
-							wirelessnetview `
-							xmind `
-							yarn `
-							youtube-dl-gui -y
+					adoptopenjdk8 `
+					androidstudio `
+					anki `
+					autohotkey `
+					bulkrenameutility`
+					calibre `
+					ccleaner `
+					choco-cleaner `
+					choco-upgrade-all-at-startup `
+					chrome-remote-desktop-chrome `
+					cpu-z `
+					crystaldiskmark `
+					ddu `
+					discord `
+					dropbox `
+					ds4windows `
+					epicgameslauncher `
+					evernote `
+					ffmpeg-batch `
+					firacodenf `
+					firefox `
+					freefilesync `
+					fzf `
+					gamesavemanager `
+					geforce-experience `
+					git `
+					google-backup-and-sync `
+					googlechrome `
+					gpu-z `
+					gradle `
+					gsudo `
+					handbrake `
+					hwmonitor `
+					insomnia-rest-api-client `
+					irfanview `
+					lame `
+					libreoffice-fresh `
+					linkshellextension `
+					lockhunter `
+					logitech-options `
+					megasync`
+					mo2 `
+					mp3gain `
+					mp3gain-gui `
+					mp3tag `
+					nodejs-lts `
+					partitionwizard `
+					picpick.portable `
+					powershell-core `
+					powertoys `
+					processhacker `
+					python3 `
+					recuva `
+					reflect-free `
+					remove-empty-directories `
+					revo-uninstaller `
+					samsung-magician`
+					samsung-nvme-driver `
+					samsung-usb-driver `
+					spotify `
+					steam `
+					steam-cleaner `
+					stremio `
+					throttlestop `
+					transmission `
+					treesizefree `
+					twitch`
+					unchecky `
+					veracrypt `
+					virtualbox `
+					virtualbox-guest-additions-guest.install `
+					vlc `
+					vscode `
+					whatsapp `
+					win32diskimager	 `
+					wirelessnetview `
+					xmind `
+					yarn `
+					youtube-dl-gui --ignore-checksums -y 
 
 [string]$currentDir = Get-Location
 [string]$WTSettings = Resolve-Path ~\AppData\Local\Packages\Microsoft.WindowsTerminalPreview*\LocalState\settings.json
@@ -121,7 +132,7 @@ Remove-Item '~\android_dev.ps1' -Force
 Remove-Item '~\kill_port.ps1' -Force
 Remove-Item '~\wsl2_network.ps1' -Force
 Remove-Item '~\.ssh' -Recurse -Force
-
+(Get-Item '~\.ssh').Delete()
 Remove-Item $WTSettings -Force
 
 # Extrai chaves SSH
@@ -173,17 +184,17 @@ reg import $currentDir'\windows_regs\Time Fix - Windows\Windows Universal Time -
 # Cria link simbolico para backups da Apple
 Write-Host "Criando um link simbolico para a pasta de backups Apple..." -ForegroundColor Green
 Remove-Item '~\AppData\Roaming\Apple Computer\MobileSync\Backup'
+New-Item -Path '~\AppData\Roaming\Apple Computer\MobileSync' -ItemType directory
 New-Item -ItemType SymbolicLink -Path '~\AppData\Roaming\Apple Computer\MobileSync\Backup' -Target 'D:\Backups\Apple'
 
 # Oh-My-Posh
 Write-Host "Instalando e configurando o Oh-My-Posh..." -ForegroundColor Green
-Remove-Item 'D:\OneDrive\Documentos\WindowsPowerShell\Microsoft.PowerShell_profile.ps1' -Force
+Remove-Item '~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1' -Force
 Install-Module PowershellGet -Force
+. Reload-Profile
 Install-Module oh-my-posh -Scope CurrentUser -AllowPrerelease -Force
 Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-Update-Module oh-my-posh -Scope CurrentUser -Force
-Update-Module posh-git -Scope CurrentUser -Force
-New-Item -ItemType SymbolicLink -Path 'D:\OneDrive\Documentos\WindowsPowerShell\Microsoft.PowerShell_profile.ps1' -Target $currentDir'\prefs\powershell_profile.ps1'
+New-Item -ItemType SymbolicLink -Path '~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1' -Target $currentDir'\prefs\powershell_profile.ps1'
 
 # Yarn Packages
 Write-Host "Instalando os pacotes Yarn..." -ForegroundColor Green
@@ -193,6 +204,7 @@ yarn global add @angular/cli react-native-cli cjs-to-es6 create-react-app json-s
 Write-Host "Baixando os plugins para o Discord..." -ForegroundColor Green
 Write-Host "É necessário instalar manualmente o Discord Better App" -ForegroundColor Yellow
 [string]$downloadDir = $env:USERPROFILE+'\AppData\Roaming\BetterDiscord\plugins\'
+New-Item -Path $downloadDir -ItemType directory
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/1Lighty/BetterDiscordPlugins/master/Plugins/BetterImageViewer/BetterImageViewer.plugin.js -OutFile $downloadDir'\BetterImageViewer.plugin.js'
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/1Lighty/BetterDiscordPlugins/master/Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js -OutFile $downloadDir'\MessageLoggerV2.plugin.js'
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/BetterFriendList/BetterFriendList.plugin.js -OutFile $downloadDir'\BetterFriendList.plugin.js'
@@ -203,3 +215,44 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/mwittrien/BetterDiscord
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/SendLargeMessages/SendLargeMessages.plugin.js -OutFile $downloadDir'\SendLargeMessages.plugin.js'
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/SpotifyControls/SpotifyControls.plugin.js -OutFile $downloadDir'\SpotifyControls.plugin.js'
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/SteamProfileLink/SteamProfileLink.plugin.js -OutFile $downloadDir'\SteamProfileLink.plugin.js'
+
+Write-Host "Instala alguns apps pré-determinados..." -ForegroundColor Green
+$exeApplications = @(
+'3uTools',
+'Battle.net',
+'Bloody7',
+'Caption',
+'DeviceRemover',
+'Discord Better App',
+'Easy Window Switcher',
+'intelhaxm-android',
+'iTunes',
+'Kaspersky Total Security',
+'MSI_Kombustor4',
+'MSIAfterburner',
+'QTTabBar',
+'RemotePlayInstaller',
+'RTSS',
+'SamsungDeX',
+'SmartSwitchPC',
+'TeraCopy Pro 3.6.0.4\TeraCopy Pro 3.6.0.4',
+'Wondershare PDFelement Professional v7.6.8.5031\pdfelement-pro_full5239',
+'G5 5590\Alienware-Command-Center-Application_J4MMM_WIN_5.2.106.0_A00_01',
+'G5 5590\Alienware-OC-Controls-Application_XXFYP_WIN64_1.3.21.1340_A00',
+'G5 5590\Dell-Power-Manager-Service_7866H_WIN64_3.7.0_A00',
+'G5 5590\Dell-Update-Application-for-Windows-10_7CKK6_WIN_4.1.0_A00'
+)
+ForEach ($item in $exeApplications) {
+	Write-Host "Instalando o aplicativo: $item" -ForegroundColor Green
+	Start-Process "D:\Mega\Aplicativos Windows\$item.exe" -Wait -Verb runas
+}
+
+$msiApplications = @(
+'CorsairHeadset',
+'IntelPowerGadget'
+)
+ForEach ($item in $msiApplications) {
+	Write-Host "Instalando o aplicativo: $item" -ForegroundColor Green
+	Start-Process "D:\Mega\Aplicativos Windows\$item.msi" -Wait -Verb runas
+}
+
